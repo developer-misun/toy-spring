38404e642676534f5a422d2c02f5b26ede7da8d6
developer-misun <developer.misun@gmail.com>
2022-10-20 16:56:26 +0900

    프로젝트 생성

---
+++ src/test/java/misun/toy/toyspring/ToySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package misun.toy.toyspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
